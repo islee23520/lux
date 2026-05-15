dcd57ea8083e5728425c5abf16043d5edd2b1dd6
ilseob lee <lysk9884@gmail.com>
2026-05-15 11:55:11 +0900

    chore(lux): extract bridge as git submodule (com.linalab.lux-bridge)

--- bridge/AiBridgeEditor/AiToolKind.cs
+++
@@ -1,9 +0,0 @@
-namespace Linalab.UnityAiBridge.Editor
-{
-    public enum AiToolKind
-    {
-        ClaudeCode,
-        OpenCode,
-        Codex
-    }
-}
--- bridge/AiBridgeEditor/UnityAiBridgeAssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Linalab.UnityAiBridge.Tests.Editor")]
